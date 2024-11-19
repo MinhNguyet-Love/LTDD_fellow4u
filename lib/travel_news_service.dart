a9a827c2a95d0e583aaaceeca2edaa13b54b8b88
Nguyễn Thị Minh Nguyệt <145785457+MinhNguyet-Love@users.noreply.github.com>
2024-11-19 17:11:24 +0700

    11

--- lib/travel_news_service.dart
+++ lib/travel_news_service.dart
@@ -1,20 +1,26 @@
-// lib/travel_news_service.dart
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'travel_news.dart';
 
 class TravelNewsService {
-  final String apiUrl = 'https://server-api-opqn.onrender.com/travel-news';
+  final String apiUrl = 'https://server-api-opqn.onrender.com/travel-news/api';
 
+  // Fetch travel news from API
   Future<List<TravelNews>> fetchTravelNews() async {
-    final response = await http.get(Uri.parse(apiUrl));
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
 
-    if (response.statusCode == 200) {
-      List jsonResponse = json.decode(response.body);
-      return jsonResponse.map((news) => TravelNews.fromJson(news)).toList();
-    } else {
-      throw Exception('Failed to load travel news');
+      if (response.statusCode == 200) {
+        // Giả sử bạn sẽ nhận một danh sách JSON
+        List jsonResponse = json.decode(response.body);
+
+        // Chuyển JSON thành đối tượng `TravelNews` bằng cách sử dụng phương thức `fromJson`
+        return jsonResponse.map((news) => TravelNews.fromJson(news)).toList();
+      } else {
+        throw Exception('Failed to load travel news');
+      }
+    } catch (e) {
+      throw Exception('Error fetching travel news: $e');
     }
   }
 }
